9f187f691176ff30117060833e066e2267af930e
Eirohou <80270541+Eirohou@users.noreply.github.com>
2025-08-12 17:13:08 +0200

    Update exampleHowToLockOutPlayersFromCertainDimensions.js

--- exampleHowToLockOutPlayersFromCertainDimensions.js
+++ exampleHowToLockOutPlayersFromCertainDimensions.js
@@ -1,10 +1,15 @@
+//Activates every tick for every player
 PlayerEvents.tick(event => {
+    //saving the player of the event in player
     const player = event.player;
+    //Checks if dimension is equal to "the end"
     if (player.level.dimension == "minecraft:the_end") {
+        //CHecks if the player is permited to be in the end
         if (!player.stages.has('EndPermited')) {
+            //Writes into chat in red that the player is not permited to enter the end
             player.tell("§cYou're not permited to enter the end!");
+            //kills the player
             player.kill();
         }
     }
 });
-
